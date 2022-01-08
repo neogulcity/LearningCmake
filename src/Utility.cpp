cc9cec38d165ea70042d296d7daacb75b699f247
neogulcity <neogulcity@gmail.com>
2022-01-08 10:13:25 +0900

    Update

--- src/Utility.cpp
+++ src/Utility.cpp
@@ -3,25 +3,5 @@
 
 namespace Utility
 {
-    void remove_blank(std::string &arg)
-    {
-        while (arg.find(" ") == 0) {
-            arg.replace(arg.begin(), arg.begin() + 1, "");
-        }
-        while (arg.rfind(" ") == arg.size() - 1) {
-            arg.replace(arg.end() - 1, arg.end(), "");
-        }
-    }
-
-    std::string remove_blank_copy(std::string arg)
-    {
-        std::string result = arg;
-        while (result.find(" ") == 0) {
-            result.replace(result.begin(), result.begin() + 1, "");
-        }
-        while (result.rfind(" ") == result.size() - 1) {
-            result.replace(result.end() - 1, result.end(), "");
-        }
-        return result;
-    }
+    
 }
--- src/Utility.h
+++ src/Utility.h
@@ -11,16 +11,4 @@ namespace Utility
             return false;
         #endif
     }
-
-    inline void remove_comments(std::string &arg)
-    {
-        int32_t index = arg.find("#") != std::string::npos ? arg.find("#") : -1;
-        if (index == -1)
-            return;
-
-        arg.replace(arg.begin() + index, arg.end(), "");
-    }
-
-    void remove_blank(std::string &arg);
-    std::string remove_blank_copy(std::string arg);
 }
